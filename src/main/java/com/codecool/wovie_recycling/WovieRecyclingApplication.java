e53983742fda2665b4326ce5b1b622c34b79ba2b
anitastipic <mailforanitastipic@gmail.com>
2023-10-19 19:46:32 +0200

    added application class

---
+++ src/main/java/com/codecool/wovie_recycling/WovieRecyclingApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.wovie_recycling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WovieRecyclingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WovieRecyclingApplication.class, args);
+    }
+
+}
