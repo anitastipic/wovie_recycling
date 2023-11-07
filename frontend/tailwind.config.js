a6e61682295ee56779ebc80fbf9a535bc783f262
anitastipic <mailforanitastipic@gmail.com>
2023-11-07 23:42:52 +0100

    added custom wovie color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                wovie: '#E8C370',
+            },
+        },
+    },
+    plugins: [],
 }
 
