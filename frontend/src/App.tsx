b2fb740935ee65d13089903613e57d31437ee3fc
anitastipic <mailforanitastipic@gmail.com>
2023-11-07 12:14:28 +0100

    added tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,9 @@ function App() {
 
     return (
         <>
-            <p>hello</p>
+            <h1 className="text-4xl font-bold underline">
+                Hello, world!
+            </h1>
         </>
     )
 }
