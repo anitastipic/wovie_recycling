b41dcfa2978e011d9d4c9db0cb891142a3bf1202
anitastipic <mailforanitastipic@gmail.com>
2023-11-07 12:32:25 +0100

    test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <>
             <h1 className="text-4xl font-bold underline">
-                Hello, world!
+                Hello!
             </h1>
         </>
     )
