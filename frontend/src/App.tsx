0a6d6a2f9dfba9fa724f01d69a66b71a9d0e812a
anitastipic <mailforanitastipic@gmail.com>
2023-11-08 17:25:31 +0100

    uninstalled react-spring web and parallax as they are buggy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ReactSpringTry from "./components/ReactSpringTry.tsx";
 
 const App: React.FC = () => {
 
     return (
-        <div className="bg-gray-800 min-h-screen">
-            <ReactSpringTry/>
+        <div className="bg-amber-50 min-h-screen">
         </div>
     )
 }
