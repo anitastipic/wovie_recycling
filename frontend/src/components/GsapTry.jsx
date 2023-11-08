40f14672a059f32fa2d3eeabba917056ac51f65d
anitastipic <mailforanitastipic@gmail.com>
2023-11-08 23:44:18 +0100

    added reat-lenis

---
+++ frontend/src/components/GsapTry.jsx
@@ -0,0 +1,46 @@
+import React, {useEffect} from "react";
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+function GsapTry() {
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const animation = gsap.timeline();
+        animation
+            .to('#waste1', {
+                scrollTrigger: {
+                    trigger: ".waste",
+                    start: "top top",
+                    toggleActions: "play pause reverse reverse",
+                },
+                y: 500,
+                duration: 10
+            });
+
+        return () => {
+            ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+        };
+    }, []);
+
+    return (
+        <>
+            <div className="w-full min-h-screen"></div>
+            <div className="gsapTry relative h-screen" id={"smooth-wrapper"}>
+                <div className="flex" id={"smooth-content"}>
+                    <img
+                        id={"waste1"}
+                        className="waste object-contain h-[350px] w-[400px]"
+                        src={"../../src/images/waste/waste1.png"}
+                        alt={""}
+                    />
+                </div>
+                <div className="h-80 w-full bg-amber-500"></div>
+                <img className="w-[870px] h-[360px] -ml-40" src="../../src/images/bin.png" alt="Bin"></img>
+            </div>
+        </>
+    );
+};
+
+export default GsapTry;
