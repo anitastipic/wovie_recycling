a14e0c19ad7be041db6f5135dd3e428ee39032d8
anitastipic <mailforanitastipic@gmail.com>
2023-11-13 23:34:35 +0100

    added function for creating and appending wasteElements

--- frontend/src/components/GsapTry.tsx
+++ frontend/src/components/GsapTry.tsx
@@ -22,25 +22,36 @@ function GsapTry() {
 
     gsap.ticker.lagSmoothing(0)
 
+    const numWaste = 15
+    const width = window.innerWidth
+    const height = window.innerHeight
+
+
+    const wasteScreenRef = useRef<HTMLDivElement>(null);
+
+    function createWasteElements() {
+        let wasteNum = 1;
+        for (let i = 0; i <= numWaste; i++) {
+            const waste = document.createElement("img");
+            const wasteClass = `waste waste${wasteNum}`
+            const wasteImgPath = `../../src/images/waste/waste${wasteNum}.png`
+            waste.setAttribute("class", wasteClass)
+            waste.setAttribute("src", wasteImgPath)
+
+            if(wasteScreenRef.current) {wasteScreenRef.current.append(waste)}
+
+            wasteNum++
+        }
+    }
+
+    useEffect(() => {
+        createWasteElements()
+    }, []);
+
+
+
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger)
-
-        const numWaste = 15
-        const width = window.innerWidth
-        const height = window.innerHeight
-
-        function createWasteElements() {
-            const wasteNum = 1;
-            for (let i = 0; i <= numWaste; i++) {
-                const waste = document.createElement("img")
-                const wasteClass = `waste waste ${wasteNum}`;
-                const wasteImgPath = `../../../src/images/waste/waste${wasteNum}`
-                waste.setAttribute("class", wasteClass);
-                waste.setAttribute("src", wasteImgPath);
-            }
-
-
-        }
 
 
         return () => {
@@ -52,7 +63,7 @@ function GsapTry() {
         <div className="gsapTryContainer h-screen">
             <div className="gsapTry">
                 <div className="h-[200vh]"/>
-                <div className=" wasteScreen bg-amber-100 flex-row overflow-scroll"/>
+                <div ref={wasteScreenRef} className=" wasteScreen bg-amber-100 flex-row overflow-scroll"/>
                 <div className="bg-blue-50 endContainer h-[200vh] w-screen relative opacity-0">
                     <div className="trigger h-1 bg-black absolute bottom-0 "/>
                 </div>
