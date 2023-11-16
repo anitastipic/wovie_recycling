031f87ee9d8afb68752b7049cd6829377143176b
anitastipic <mailforanitastipic@gmail.com>
2023-11-16 19:49:41 +0100

    added security configuration

--- backend/src/main/java/com/codecool/wovie_recycling/configuration/SecurityConfig.java
+++ backend/src/main/java/com/codecool/wovie_recycling/configuration/SecurityConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +18,10 @@ public class SecurityConfig {
                  .csrf(AbstractHttpConfigurer::disable)
                  .authorizeHttpRequests(request -> request.anyRequest().permitAll())
                  .build();
+     }
+
+     @Bean
+     public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
      }
 }
