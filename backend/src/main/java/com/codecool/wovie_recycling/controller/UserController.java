b6ee635ce727627f79c4b8a94b8574b6544343af
anitastipic <mailforanitastipic@gmail.com>
2023-11-16 19:52:24 +0100

    added repo, service and controller to handle login of user

---
+++ backend/src/main/java/com/codecool/wovie_recycling/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.codecool.wovie_recycling.controller;
+
+import com.codecool.wovie_recycling.model.User;
+import com.codecool.wovie_recycling.repository.UserRepository;
+import com.codecool.wovie_recycling.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    private final UserService userService;
+
+    public UserController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, UserService userService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+    }
+
+    @Transactional
+    @PostMapping("/register")
+    public String registerNewUserAccount(@RequestBody User user) {
+        return userService.createNewUser(user);
+    }
+
+    @PostMapping("/login")
+
+
+    private boolean emailExists(String email) {
+        return userRepository.findByUsername(email) != null;
+    }
+}
--- backend/src/main/java/com/codecool/wovie_recycling/service/UserService.java
+++ backend/src/main/java/com/codecool/wovie_recycling/service/UserService.java
@@ -1,7 +1,41 @@
 package com.codecool.wovie_recycling.service;
 
+import com.codecool.wovie_recycling.model.User;
+import com.codecool.wovie_recycling.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, TokenService tokenService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
+    }
+
+    public String createNewUser(User user) {
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        saveUser(user);
+        return "User created!";
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User authenticate(String username, String password) {
+        User user = userRepository.findByUsername(username);
+        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
+            return user;
+        }
+        return null;
+    }
+
+
 }
