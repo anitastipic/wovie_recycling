255d836946dba22fe9f380aa549084f877b64ec3
anitastipic <mailforanitastipic@gmail.com>
2023-12-05 14:33:59 +0100

    testcommit for worklow changes

--- backend/src/main/java/com/codecool/wovie_recycling/controller/DistrictController.java
+++ backend/src/main/java/com/codecool/wovie_recycling/controller/DistrictController.java
@@ -4,15 +4,13 @@ import com.codecool.wovie_recycling.model.District;
 import com.codecool.wovie_recycling.service.DistrictService;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("district")
+@CrossOrigin(origins = "http://localhost:5173")
 public class DistrictController {
     private final DistrictService districtService;
 
