ee8d9e5d89308fbed96890945628e6c88cb29fe1
anitastipic <mailforanitastipic@gmail.com>
2024-03-22 21:30:19 +0100

    removed vienna scroll animation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar.js";
-import TypeWriter from "../components/TypeWriter.tsx";
-import ViennaVideo from "../components/ViennaVideo.tsx";
+import arrow from "../assets/Arrow.svg";
 
 
 export default function Home() {
@@ -9,22 +8,21 @@ export default function Home() {
     const homeText = ["Auch immer auf der Suche nach Containern in deiner Nähe?", "Kein Problem - Wir helfen dir!", "WoVie - Wo und Wie recyceln in Wien."];
     const typingDelay = 75;
     const nextLineDelay = 1500;
+    const style = "text-[2.5vw] font-primary font-bold text-amber-50";
 
     return (
         <div className="bg-third h-[1000vh]">
-            <div className="fixed z-20">
-                <Navbar/>
-            </div>
-            <div className="flex flex-col items-center justify-evenly fixed">
-                <div className="w-screen opacity-60 z-0 absolute top-0 left-0">
-                    <ViennaVideo/>
+            <Navbar/>
+            <div className="grid grid-cols-1 pt-[16vh]">
+                <div className="h-[20vh] content-center text-center">
+                    <p className="text-[2.5vw] font-primary font-bold text-amber-50">WoVie - Wo und Wie recyceln in
+                        Wien</p>
                 </div>
-                <div className="absolute top-[45vh] left-[20vw] w-[60vw] z-10 text-center">
-                    <TypeWriter
-                        text={homeText}
-                        typingDelay={typingDelay}
-                        nextWordDelay={nextLineDelay}
-                    />
+                <div className="bg-green-500">
+                    <img className="" src={"/Vienna.png"}/>
+                </div>
+                <div className="h-[20vh] w-[100vw] flex justify-center items-center">
+                    <img className="h-[4vh]" src={arrow}/>
                 </div>
             </div>
         </div>
