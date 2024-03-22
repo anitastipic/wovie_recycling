cff02eda5e46718a3ebc0a20905be3ee5d9a7f3f
anitastipic <mailforanitastipic@gmail.com>
2024-03-22 14:31:42 +0100

    added layout component and refactored home component correspondingly

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import UserSignUp from "../components/UserSignUp.js";
 import Navbar from "../components/Navbar.js";
-import MapVideo from "../components/MapVideo.tsx";
 import TypeWriter from "../components/TypeWriter.tsx";
-import GsapTry from "../components/GsapTry.tsx";
 import ViennaVideo from "../components/ViennaVideo.tsx";
 
 
 export default function Home() {
 
-    const words = ["Auch immer auf der Suche nach Containern in deiner Nähe?", "Kein problem - Wir helfen dir!", "WoVie - Wo und Wie recyceln in Wien."];
+    const homeText = ["Auch immer auf der Suche nach Containern in deiner Nähe?", "Kein Problem - Wir helfen dir!", "WoVie - Wo und Wie recyceln in Wien."];
     const typingDelay = 75;
     const nextLineDelay = 1500;
 
@@ -19,12 +16,12 @@ export default function Home() {
                 <Navbar/>
             </div>
             <div className="flex flex-col items-center justify-evenly fixed">
-                <div className="opacity-20 z-0 absolute top-0 left-0">
+                <div className="w-screen opacity-60 z-0 absolute top-0 left-0">
                     <ViennaVideo/>
                 </div>
-                <div className="absolute top-[45vh] left-[20vw] w-[60vw] z-10">
+                <div className="absolute top-[45vh] left-[20vw] w-[60vw] z-10 text-center">
                     <TypeWriter
-                        text={words}
+                        text={homeText}
                         typingDelay={typingDelay}
                         nextWordDelay={nextLineDelay}
                     />
