485227863f76e2c432212435d5da3f1f64e0822b
anitastipic <mailforanitastipic@gmail.com>
2024-03-22 21:12:05 +0100

    fixed placement of navbar, removed padding and added bg color

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from '../images/logos/WoVie-logo-weiß.png';
 import heroBadge from '../assets/HeroBadge.svg'
-import heroBadgeS from '../assets/HeroBadgeSmall.svg'
 import {Link} from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="h-[13vh] w-full px-10 pt pt-6 flex items-center fixed justify-between">
+        <nav className="h-[16vh] w-full px-10 flex items-center fixed justify-between bg-third">
             <Link to="/" className="flex h-[13vh] w-auto min-w-[20vw] justify-self-start">
                 <img src={logo} alt="Logo" className="object-contain"/>
             </Link>
@@ -22,7 +21,7 @@ export default function Navbar() {
                     <a className="text-amber-50 text-lg transition-colors duration-300 transform hover:text-wovie mx-3"
                        href="/contact">Kontakt</a>
                 </div>
-                <Link to="/login" className=" h-[6.5vh] flex w-auto">
+                <Link to="/hero" className=" h-[6.5vh] flex w-auto">
                     <img src={heroBadge} alt="Hero" className="object-contain"/>
                 </Link>
             </div>
