b45a03580a4ec9cc396f4d6f3de30e35eb53517f
anitastipic <mailforanitastipic@gmail.com>
2024-03-22 23:45:21 +0100

    removed unused colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,13 +3,31 @@ export default {
     content: ["./src/**/*.{html,js,jsx,tsx}"],
     theme: {
         extend: {
+            textShadow: {
+                sm: '0 1px 2px var(--tw-shadow-color)',
+                DEFAULT: '0 10px 5px var(--tw-shadow-color)',
+                lg: '0 8px 16px var(--tw-shadow-color)',
+            },
+            fontFamily: {
+                primary: ["Oswald", "sans-serif"]
+            },
             colors: {
                 wovie: '#E8C370',
-                second: '#5E796A',
                 third: '#1E313B',
             },
         },
     },
-    plugins: [],
+    plugins: [
+        function ({ matchUtilities, theme }) {
+            matchUtilities(
+                {
+                    'text-shadow': (value) => ({
+                        textShadow: value,
+                    }),
+                },
+                { values: theme('textShadow') }
+            )
+        },
+    ],
 }
 
