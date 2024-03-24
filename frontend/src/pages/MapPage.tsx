a9a5a08b73c546e7e5950c7623c6f95dcda2a460
anitastipic <mailforanitastipic@gmail.com>
2024-03-24 13:52:53 +0100

    bg adjustment

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -3,11 +3,11 @@ import Navbar from "../components/Navbar.tsx";
 
 export default function MapPage() {
     return (
-        <div className="bg-third h-screen scroll-m-0">
+        <div className="bg-third h-screen w-full scroll-m-0">
             <div>
                 <Navbar></Navbar>
             </div>
-            <div className="pt-[22vh]">
+            <div className="pt-[22vh] w-full">
                 <Map/>
             </div>
         </div>
