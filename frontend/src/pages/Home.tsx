f1aa35b3b09eb7a116e9bea5dc56a2064c5d1708
anitastipic <mailforanitastipic@gmail.com>
2024-03-24 14:28:04 +0100

    toggeled visibility of scroll arrow for scrolling effect and mobile version

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Navbar from "../components/Navbar.js";
 import arrow from "../assets/Arrow.svg";
 import wave from "../assets/wavyBackground.svg";
@@ -8,17 +8,34 @@ import {useNavigate} from "react-router-dom";
 
 export default function Home() {
 
-    const homeText = ["Auch immer auf der Suche nach Containern in deiner Nähe?", "Kein Problem - Wir helfen dir!", "WoVie - Wo und Wie recyceln in Wien."];
-    const typingDelay = 75;
-    const nextLineDelay = 1500;
-    const style = "text-[2.5vw] font-primary font-bold text-amber-50";
-
+    const [arrowIsVisible, setArrowIsVisible] = useState(true);
+    const [scrollHeight, setScrollHeight] = useState(0);
     const nav = useNavigate();
+
+    useEffect(() => {
+        window.addEventListener("scroll", listenToScroll);
+        return () =>
+            window.removeEventListener("scroll", listenToScroll);
+    }, [])
+
+    function listenToScroll() {
+        const heightToHideFrom = 50;
+        const currentScrollPosition = document.body.scrollTop ||
+            document.documentElement.scrollTop;
+        setScrollHeight(currentScrollPosition);
+
+        if (currentScrollPosition > heightToHideFrom) {
+            arrowIsVisible && setArrowIsVisible(false);
+        } else {
+            setArrowIsVisible(true);
+        }
+
+    }
 
     return (
         <div className="bg-third h-[216%]">
             <Navbar/>
-            <div className="grid grid-cols-1 pt-[16vh] h-[100%]">
+            <div className="grid grid-cols-1 pt-[16vh] h-[120%] md:h[100%]">
                 <div className="h-[20vh] content-center text-center">
                     <p className="text-[2.5vw] font-primary font-bold text-amber-50">WoVie - Wo und Wie recyceln in
                         Wien</p>
@@ -26,9 +43,13 @@ export default function Home() {
                 <div className="">
                     <img className="" src={"/Vienna.png"}/>
                 </div>
+
                 <div className="h-[20vh] w-[100vw] flex justify-center items-center">
-                    <img className="h-[4vh]" src={arrow}/>
+                    {arrowIsVisible &&
+                        <img className="h-[4vh] invisible md:visible" src={arrow}/>
+                    }
                 </div>
+
             </div>
             <div className="h-[100%] relative">
                 <div className="absolute top-[15vw] ml-[12vw]">
@@ -40,7 +61,10 @@ export default function Home() {
                 <img className="absolute h-[15vw] top-[37vw] left-[59.5vw]" src="/trashcanYellow.png"/>
                 <img className="absolute h-[9vw] top-[41.5vw] left-[74.5vw]" src="/trashcanRed.png"/>
                 <img className="absolute h-[11.5vw] top-[39.8vw] left-[86.5vw]" src="/trashcanBrown.png"/>
-                <button className="absolute top-[45.5vw] left-[17vw] h-[5vw] w-[15vw] bg-third rounded-full text-[2vw] text-amber-50" onClick={() => nav('/map')}>Zur Karte</button>
+                <button
+                    className="absolute top-[45.5vw] left-[17vw] h-[5vw] w-[15vw] bg-third rounded-full text-[2vw] text-amber-50"
+                    onClick={() => nav('/map')}>Zur Karte
+                </button>
             </div>
         </div>
     )
